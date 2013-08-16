37e9034d7928b678104e5bbae7bc683f641edea9
Andrey Koleshko <ka8725@gmail.com>
2013-08-16 23:20:36 +0300

    Editing items

--- js/controllers/todo_controller.js
+++ js/controllers/todo_controller.js
@@ -1,7 +1,18 @@
 Todos.TodoController = Ember.ObjectController.extend({
+  isEditing: false,
+
   removeTodo: function() {
     var todo = this.get('model');
     todo.deleteRecord();
     todo.save();
+  },
+
+  editTodo: function () {
+    this.set('isEditing', true);
+  },
+
+  acceptChanges: function () {
+    this.set('isEditing', false);
+    this.get('model').save();
   }
 });
