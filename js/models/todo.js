1244e73a4468779e9f9af8b709091b8861e9fbc5
Andrey Koleshko <ka8725@gmail.com>
2013-08-19 11:49:29 +0300

    Fix dates

--- js/models/todo.js
+++ js/models/todo.js
@@ -8,19 +8,19 @@ Todos.Todo.FIXTURES = [
  {
    id: 1,
    title: 'Learn Ember.js',
-   dueDate: new Date('27/08/2013'),
+   dueDate: new Date('2013-08-27'),
    priority: 2
  },
  {
    id: 2,
    title: 'Have an interview',
-   dueDate: new Date('19/08/2013'),
+   dueDate: new Date('2013-08-19'),
    priority: 1
  },
  {
    id: 3,
    title: 'Visit parents',
-   dueDate: new Date('17/08/2013'),
+   dueDate: new Date('2013-08-17'),
    priority: 0
  }
 ];
