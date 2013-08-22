4d7c1dc3d759b236c6ddb61cf7e743c35fb58173
Andrey Koleshko <ka8725@gmail.com>
2013-08-22 16:31:16 +0300

    Remove waste of code which removes record

--- public/js/controllers/todo_controller.js
+++ public/js/controllers/todo_controller.js
@@ -1,22 +1,7 @@
 App.TodoController = Ember.ObjectController.extend({
-  isEditing: false,
-
-  saveTodo: function() {
-    this.get('store').commit();
-    this.set('isEditing', false);
-  },
-
   removeTodo: function() {
     var todo = this.get('model');
     todo.deleteRecord();
     this.get('store').commit();
-  },
-
-  editTodo: function () {
-    this.set('isEditing', true);
-  },
-
-  acceptChanges: function () {
-    this.set('isEditing', false);
   }
 });
