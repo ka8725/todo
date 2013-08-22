5cb3003979a4827118c223e9e8115e63aa545c00
Andrey Koleshko <ka8725@gmail.com>
2013-08-22 18:29:09 +0300

    Rename Rakefile and remove old code

--- public/js/controllers/todos_controller.js
+++ public/js/controllers/todos_controller.js
@@ -8,25 +8,5 @@ App.TodosController = Ember.ArrayController.extend({
     }, this);
 
     this.get('store').commit();
-  },
-
-  createTodo: function(todo) {
-    var data = this.getProperties('title', 'priority', 'due_date');
-    var todo = App.Todo.createRecord(data);
-
-    var self = this;
-
-    todo.on('becameInvalid', function(todo) {
-      // show errors on the form. code goes here
-      console.log(todo.errors);
-    });
-
-    todo.on('didCreate', function() {
-      // render list. code goes here
-      self.set('title', '');
-      self.set('priority', '');
-      self.set('due_date', '');
-    });
-    todo.save();
   }
 });
