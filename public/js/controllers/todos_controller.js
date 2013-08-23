1cafce65a9c0bb6d5ba944a03db35d8f57962975
Andrey Koleshko <ka8725@gmail.com>
2013-08-23 13:29:11 +0300

    Sort items

--- public/js/controllers/todos_controller.js
+++ public/js/controllers/todos_controller.js
@@ -8,5 +8,26 @@ App.TodosController = Ember.ArrayController.extend({
     }, this);
 
     this.get('store').commit();
+  },
+
+  sortedContent: (function() {
+    var content;
+    content = this.get("content") || [];
+    return Ember.ArrayProxy.createWithMixins(Ember.SortableMixin, {
+      content: content.toArray(),
+      sortProperties: this.get('sortProperties'),
+      sortAscending: this.get('sortAscending')
+    });
+  }).property("content.@each", 'sortProperties', 'sortAscending'),
+
+  doSort: function(sortBy) {
+    var previousSortBy;
+    previousSortBy = this.get('sortProperties.0');
+    if (sortBy === previousSortBy) {
+      return this.set('sortAscending', !this.get('sortAscending'));
+    } else {
+      set('sortAscending', true);
+      return this.set('sortProperties', [sortBy]);
+    }
   }
 });
